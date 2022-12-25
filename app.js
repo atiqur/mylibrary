d5d1dfe18cf125c6e8bbaa85a367acfa1bdaa24c
Atiqur Rahman <atiqur.03@gmail.com>
2022-12-25 23:06:42 +0530

    First working copy

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-const bookTitle = document.querySelector('#bookInputTitle');
-const bookAuthor = document.querySelector('#bookInputAuthor');
-const bookIsRead = document.querySelector('#bookIsRead');
-const listGroup = document.querySelector('.list-group');
+const bookTitle = document.querySelector("#bookInputTitle")
+const bookAuthor = document.querySelector("#bookInputAuthor")
+const bookIsRead = document.querySelector("#bookIsRead")
+const listGroup = document.querySelector(".list-group")
 
-const submitBtn = document.querySelector('#addBookSubmitBtn');
+const submitBtn = document.querySelector("#addBookSubmitBtn")
 
-submitBtn.addEventListener('click', addBookToLibrary);
+submitBtn.addEventListener("click", addBookToLibrary)
 
-let myLibrary = [];
+let myLibrary = []
 
 function Book(title = undefined, author = undefined, isRead = false) {
-  this.title = title;
-  this.author = author;
-  this.isRead = isRead;
+  this.title = title
+  this.author = author
+  this.isRead = isRead
 }
 
 function addBookToLibrary(e) {
-  const book = new Book(bookTitle.value, bookAuthor.value, bookIsRead.checked);
-  myLibrary.push(book);
-  addBookToList();
-  e.preventDefault();
+  const book = new Book(bookTitle.value, bookAuthor.value, bookIsRead.checked)
+  myLibrary.push(book)
+  addBookToList()
+  e.preventDefault()
 }
 
 function addBookToList() {
+  let li = ""
   myLibrary.forEach((book) => {
-    listGroup.appendChild(document.ap`
+    li += `
     <li class="list-group-item">
       <p>Title: ${book.title}</p>
       <p>Author: ${book.author}</p>
       <p>Read: ${book.isRead}</p>
     </li>
-    `);
-  });
+    `
+  })
+  listGroup.innerHTML = li
 }
