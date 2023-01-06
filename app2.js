f6b1606942ad9b27ca300263c409779fadbbd2b2
Atiqur Rahman <atiqur.03@gmail.com>
2023-01-06 22:08:50 +0530

    Update and delete book

--- app2.js
+++ app2.js
@@ -12,7 +12,7 @@ const ItemCtrl = (function () {
 
   const library = {
     books: [],
-    currentItem: null,
+    currentBookIndex: null,
   }
 
   return {
@@ -28,8 +28,36 @@ const ItemCtrl = (function () {
 
       return newBook
     },
+    getCurrentBookIndex: function () {
+      return library.currentBookIndex
+    },
+    setCurrentBookIndex: function (index) {
+      library.currentBookIndex = index
+    },
+    deleteBook: function (id) {
+      const ids = library.books.map((book) => book.id)
+      const index = ids.indexOf(id)
+      library.books.splice(index, 1)
+    },
+    updateBook: function (id) {
+      const book = ItemCtrl.getBookById(id)
+      book.title = UICtrl.getBookTitle()
+      book.author = UICtrl.getAuthor()
+      book.isRead = UICtrl.getIsRead()
+      return book
+    },
     getBooks: function () {
       return library.books
+    },
+    getBookById: function (id) {
+      let found
+
+      library.books.forEach((book) => {
+        if (book.id === id) {
+          found = book
+        }
+      })
+      return found
     },
     logData: function () {
       return library
@@ -43,8 +71,11 @@ const UICtrl = (function () {
     inputTitle: "#bookInputTitle",
     inputAuthor: "#bookInputAuthor",
     isRead: "#bookIsRead",
+    deleteBook: ".deleteBookBtn",
+    editBook: ".editBookBtn",
     listGroup: ".list-group",
     submitBtn: "#addBookSubmitBtn",
+    updateBtn: "#updateBookSubmitBtn",
   }
   return {
     getSelectors: function () {
@@ -63,19 +94,52 @@ const UICtrl = (function () {
       let li = document.createElement("li")
       li.className = "list-group-item"
       li.id = `item-${book.id}`
-      li.innerHTML = `<p>Title: ${book.title}</p>
-            <p>Author: ${book.author}</p>
-            <p>Read: ${book.isRead}</p>
-            <button type="button" class="btn btn-success" id="editBookBtn">Edit</button>
-            <button type="button" class="btn btn-danger" id="deleteBookBtn">Delete</button>`
+      li.innerHTML = `<p class="title">Title: ${book.title}</p>
+            <p class="author">Author: ${book.author}</p>
+            <p class="isRead">Read: ${book.isRead}</p>
+            <button type="button" class="btn btn-success editBookBtn">Edit</button>
+            <button type="button" class="btn btn-danger deleteBookBtn">Delete</button>`
       document
         .querySelector(UISelectors.listGroup)
         .insertAdjacentElement("beforeend", li)
     },
-    showBookList: function (books) {
+    putInInputFields: function (book) {
+      document.querySelector(UISelectors.inputTitle).value = book.title
+      document.querySelector(UISelectors.inputAuthor).value = book.author
+      document.querySelector(UISelectors.isRead).checked = book.isRead
+    },
+    showUpdateBtn: function () {
+      document.querySelector(UISelectors.updateBtn).style.display = "inline"
+    },
+    hideUpdateBtn: function () {
+      document.querySelector(UISelectors.updateBtn).style.display = "none"
+    },
+    showAddBtn: function () {
+      document.querySelector(UISelectors.submitBtn).style.display = "inline"
+    },
+    hideAddBtn: function () {
+      document.querySelector(UISelectors.submitBtn).style.display = "none"
+    },
+
+    showUpdatedBook: function (book) {
+      const bookId = `#item-${book.id}`
+      const bookToUpdate = document.querySelector(bookId)
+      bookToUpdate.querySelector(".title").textContent = `Title: ${book.title}`
+      bookToUpdate.querySelector(
+        ".author"
+      ).textContent = `Author: ${book.author}`
+      bookToUpdate.querySelector(".isRead").textContent = `Read: ${book.isRead}`
+    },
+    showBookList: function () {
+      const books = ItemCtrl.getBooks()
       books.forEach((book) => {
         UICtrl.addBookToList(book)
       })
+    },
+    deleteBookFromList: function (id) {
+      const bookId = `#item-${id}`
+      const bookToDelete = document.querySelector(bookId)
+      bookToDelete.remove()
     },
     clearInput: function () {
       document.querySelector(UISelectors.inputTitle).value = ""
@@ -87,14 +151,22 @@ const UICtrl = (function () {
 
 // App Controller
 const App = (function (ItemCtrl, UICtrl) {
+  const UISelectors = UICtrl.getSelectors()
   const loadEventListners = function () {
-    const UISelectors = UICtrl.getSelectors()
     document
       .querySelector(UISelectors.submitBtn)
-      .addEventListener("click", bookAddBtn)
+      .addEventListener("click", bookAddBtnClicked)
+
+    document
+      .querySelector(UISelectors.listGroup)
+      .addEventListener("click", editBookBtnClicked)
+
+    document
+      .querySelector(UISelectors.updateBtn)
+      .addEventListener("click", bookUpdateBtnClicked)
   }
 
-  const bookAddBtn = function (e) {
+  const bookAddBtnClicked = function (e) {
     const bookTitle = UICtrl.getBookTitle()
     const author = UICtrl.getAuthor()
     const isRead = UICtrl.getIsRead()
@@ -104,8 +176,48 @@ const App = (function (ItemCtrl, UICtrl) {
 
       UICtrl.addBookToList(newBook)
       UICtrl.clearInput()
+    } else {
+      alert("Please provide a valid input.")
     }
     e.preventDefault()
+  }
+
+  const bookUpdateBtnClicked = function (e) {
+    const id = ItemCtrl.getCurrentBookIndex()
+    const updatedBook = ItemCtrl.updateBook(id)
+    UICtrl.showUpdatedBook(updatedBook)
+    UICtrl.hideUpdateBtn()
+    UICtrl.showAddBtn()
+    UICtrl.clearInput()
+    e.preventDefault()
+  }
+
+  const editBookBtnClicked = function (e) {
+    const bookId = e.target.parentNode.id
+    const bookArr = bookId.split("-")
+    const id = parseInt(bookArr[1])
+    const bookToEdit = ItemCtrl.getBookById(id)
+    if (e.target.classList.contains("editBookBtn")) {
+      editBook(bookToEdit)
+    } else if (e.target.classList.contains("deleteBookBtn")) {
+      deleteBook(id)
+    }
+    e.preventDefault()
+  }
+
+  const editBook = function (book) {
+    ItemCtrl.setCurrentBookIndex(book.id)
+    UICtrl.hideAddBtn()
+    UICtrl.showUpdateBtn()
+    UICtrl.putInInputFields(book)
+  }
+
+  const deleteBook = function (id) {
+    const ok = confirm("Do you really want to delete this book?")
+    if (ok) {
+      ItemCtrl.deleteBook(id)
+      UICtrl.deleteBookFromList(id)
+    }
   }
 
   return {
